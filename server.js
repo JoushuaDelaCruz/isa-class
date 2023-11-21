cc56a3d7823b674df24a514f9ba27cd09334adb5
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-11-21 09:40:18 -0800

    Ready to deploy

--- server.js
+++ server.js
@@ -8,10 +8,7 @@ const server = http.createServer((req, res) => {
   // if you run the client from sth like file:///C:/nodejs/10httonlyCookie1/index.html
   // it would mean HTML file is being served via the file:// protocol directly from your filesystem rather than over HTTP or HTTPS.
   console.log("Request origin:", req.headers.origin);
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://655ce67a6f5e5515391c691d--ubiquitous-taffy-4e6ba3.netlify.app"
-  ); // Adjust the port if necessary
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5500"); // Adjust the port if necessary
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
@@ -32,9 +29,10 @@ const server = http.createServer((req, res) => {
       try {
         const { username, password } = JSON.parse(body); // Parse the JSON body
         // Change password to length of 6
+        console.log(process.env.USERNAME, process.env.PASSWORD);
         if (
-          username === process.env.USERNAME &&
-          password === process.env.PASSWORD
+          username === process.env.SERVER_USERNAME &&
+          password === process.env.SERVER_PASSWORD
         ) {
           const payload = {
             username: username,
